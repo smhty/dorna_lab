ab0a8026f6edda9893664656967b989ff1aaa032
smhty <hussein.ty@gmail.com>
2022-06-03 21:56:35 -0700

    Update start.sh

--- dorna_lab/start.sh
+++ dorna_lab/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # run the program
-/home/dorna/Downloads/dorna_venv/bin/python3 /home/dorna/Downloads/dorna_lab/dorna_lab/application.py
+#/home/dorna/Downloads/dorna_venv/bin/python3 /home/dorna/Downloads/dorna_lab/dorna_lab/application.py
+source /home/dorna/Downloads/dorna_venv/bin/activate
+python3 /home/dorna/Downloads/dorna_lab/dorna_lab/application.py
