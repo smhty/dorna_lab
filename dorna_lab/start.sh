7bf44c2ce46e4d8e9c9375af0b9f7a7f9587caef
smhty <hussein.ty@gmail.com>
2022-06-07 08:50:42 -0700

    bash

--- dorna_lab/start.sh
+++ dorna_lab/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 # sleep for 5 seconds
 sleep 5
+
 # run the program
-#/home/dorna/Downloads/dorna_venv/bin/python3 /home/dorna/Downloads/dorna_lab/dorna_lab/application.py
 python3 /home/dorna/Downloads/dorna_lab/dorna_lab/application.py
