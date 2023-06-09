fddebabe5a297163691b25f8614d1a2d3246c937
smhty <hussein.ty@gmail.com>
2023-06-09 13:02:07 -0700

    homing direction

--- dorna_lab/static/js/home.js
+++ dorna_lab/static/js/home.js
@@ -2,11 +2,12 @@ $(".home_joint_b").on("click", function(e) {
 	e.preventDefault();
 	
 	let index = $(this).attr("data-key")
-	let value = Number($(`.set_joint_v[index=${index}]`).prop("value"))		
+	let value = Number($(`.set_joint_v[index=${index}]`).prop("value"))
+	let dir = $(this).attr("data-dir")			
 	
 	let msg = {
 	  "_server": "shell",
-	  "prm":  ["sudo python3 /home/dorna/Downloads/dorna_python/example/home/main.py --Host localhost --Index "+index+ " --Value "+value],
+	  "prm":  ["sudo python3 /home/dorna/Downloads/dorna_python/example/home/main.py --Host localhost --Index "+index+ " --Value "+value+ " --Dir "+dir ],
 
 	}
 	send_message(msg)
