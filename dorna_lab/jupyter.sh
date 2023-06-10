634fcba43a8a3e138fc2392b0b5cb43d4f7dc16d
smhty <hussein.ty@gmail.com>
2023-06-09 19:48:11 -0700

    jupyter

--- dorna_lab/jupyter.sh
+++ dorna_lab/jupyter.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 # sleep for 5 seconds
-sleep 10
+sleep 5
 
 # run notebook
-jupyter notebook --ip 0.0.0.0 --no-browser --port=8888 --allow-root --notebook-dir="/home/dorna/Projects/" --NotebookApp.token='' --NotebookApp.password=''
+#jupyter notebook --ip 0.0.0.0 --no-browser --port=8888 --allow-root --notebook-dir="/home/dorna/Projects/" --NotebookApp.token='' --NotebookApp.password=''
+
+# Check if Jupyter server is running
+if jupyter notebook list &> /dev/null; then
+	echo "Jupyter server is already running."
+else
+	echo "Starting Jupyter server..."
+	# run notebook
+	pyter notebook --ip 0.0.0.0 --no-browser --port=8888 --allow-root --notebook-dir="/home/dorna/Projects/" --NotebookApp.token='' --NotebookApp.password='' &
+fi
