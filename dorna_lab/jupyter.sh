9901b7e5870ff6e86f697ae026cd02371a34e285
smhty <hussein.ty@gmail.com>
2023-06-20 11:51:58 -0700

    Update jupyter.sh

--- dorna_lab/jupyter.sh
+++ dorna_lab/jupyter.sh
@@ -4,4 +4,5 @@
 sleep 10
 
 # run notebook
-jupyter notebook --ip 0.0.0.0 --no-browser --port=8888 --allow-root --notebook-dir="/home/dorna/" --NotebookApp.token='' --NotebookApp.password=''
+#jupyter notebook --ip 0.0.0.0 --no-browser --port=8888 --allow-root --notebook-dir="/home/dorna/" --NotebookApp.token='' --NotebookApp.password=''
+nohup jupyter notebook --ip 0.0.0.0 --no-browser --port=8888 --allow-root --notebook-dir="/home/dorna/" --NotebookApp.token='' --NotebookApp.password='' &
