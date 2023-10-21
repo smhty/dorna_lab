4cd65fa0fbbc182e90f3543c36962451b9f7de1b
javad <smjty25@gmail.com>
2023-10-21 09:50:24 +0330

    cam functinon update

--- dorna_lab/tool/camera.py
+++ dorna_lab/tool/camera.py
@@ -7,32 +7,31 @@ class Camera(object):
 	camera_thread_stop = False
 
 	def __init__(self):
-		self.cap  = cv2.VideoCapture(0) 
-		frame_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-		frame_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-		frame_rate = self.cap.get(cv2.CAP_PROP_FPS)
+		self.cap_r  = cv2.VideoCapture(0) #these two must be changed for webcam
+		self.cap_l  = cv2.VideoCapture(0) 
+
+		frame_width = int(self.cap_r.get(cv2.CAP_PROP_FRAME_WIDTH))
+		frame_height = int(self.cap_r.get(cv2.CAP_PROP_FRAME_HEIGHT))
+		frame_rate = self.cap_r.get(cv2.CAP_PROP_FPS)
 
 		print(f"Frame Width: {frame_width}")
 		print(f"Frame Height: {frame_height}")
 		print(f"Frame Rate: {frame_rate} FPS")
 
-	def get_frame(self):
-		return self.cap.read()
-
 	async  def capture_webcam_data(self,ws_handler):
 		while not self.camera_thread_stop:
-			ret, frame = self.cap.read()
-			
-			#cv2.imshow("Webcam Preview", frame)
-			
+			ret_r, frame_r = self.cap_r.read()
+			#ret_l, frame_l = self.cap_l.read()			
 
 			#Process and encode the frame (e.g., to JPEG)
-			_, encoded_image = cv2.imencode('.jpg', frame)
+			_, encoded_image = cv2.imencode('.jpg', frame_r)
 			image_data = base64.b64encode(encoded_image.tobytes()).decode('utf-8')
 			ws_handler.write_message({'to':"cam_r",'image_data': image_data})
+
+
+			#_, encoded_image = cv2.imencode('.jpg', frame_l)
+			#image_data = base64.b64encode(encoded_image.tobytes()).decode('utf-8')
 			ws_handler.write_message({'to':"cam_l",'image_data': image_data})
 
-			#key = cv2.waitKey(1)
-			#if key & 0xFF == ord('q'):
-			#	break
-			await asyncio.sleep(0.1)
+
+			await asyncio.sleep(0.01)
